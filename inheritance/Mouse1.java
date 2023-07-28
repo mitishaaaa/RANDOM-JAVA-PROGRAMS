4b34e820742a377b395c14d0d371c3bb443d86d1
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-07-29 01:23:32 +0530

    Update Mouse1.java

--- inheritance/Mouse1.java
+++ inheritance/Mouse1.java
@@ -1,10 +1,6 @@
-public class Mouse{
-  public static void leftClick(){
-    System.out.println("Click!");
-  }
-  public static void rightClick(){
-    System.out.println("CLick!");
-  }
-  public static void scrollUp(){
-    System.out.println("Scrolled up!");
-  }
+public class Mouse1{
+String texture = "Matte";
+  public static void setColor(String color){
+    System.out.println(color);
+     }  
+}
