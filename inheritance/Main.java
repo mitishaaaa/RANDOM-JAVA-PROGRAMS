46d921a1d67fb2cda514119b50e7132b69807662
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-07-29 01:20:03 +0530

    Update Main.java

--- inheritance/Main.java
+++ inheritance/Main.java
@@ -1,7 +1,13 @@
 public class main{
   public static void main (String[] args){
 
-Mouse2 = m2
+Mouse2 m2 = new Mouse2();
+    m2.leftClick();
+    m2.rightCLick();
+    m2.scrollDown();
+    m2.scrollUp();
+
+    m2.connect();
 
     
   }
