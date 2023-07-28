01b728cd48effa40f111cb7d9d6770e6083646c5
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-07-29 01:30:42 +0530

    Update Mouse3.java

--- inheritance/Mouse3.java
+++ inheritance/Mouse3.java
@@ -1,11 +1,4 @@
 
-public class Mouse{
-  public static void leftClick(){
-    System.out.println("Click!");
-  }
-  public static void rightClick(){
-    System.out.println("CLick!");
-  }
-  public static void scrollUp(){
-    System.out.println("Scrolled up!");
-  }
+public class Mouse3 extends Mouse{
+  boolean ambidextrous = true;
+}
