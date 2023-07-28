941d7b7d0df6c055c92c59ec1eeb105ba5a5d6fc
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-07-29 01:28:11 +0530

    Update Mouse2.java

--- inheritance/Mouse2.java
+++ inheritance/Mouse2.java
@@ -1,11 +1,6 @@
 
-public class Mouse{
-  public static void leftClick(){
-    System.out.println("Click!");
-  }
-  public static void rightClick(){
-    System.out.println("CLick!");
-  }
-  public static void scrollUp(){
-    System.out.println("Scrolled up!");
-  }
+public class Mouse2 extends Mouse{
+public static void connect(){
+System.out.println("connected");
+}  
+}
