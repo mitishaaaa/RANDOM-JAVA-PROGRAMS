a71b78fb3d0e32838d3f70997875c15b6a539c26
Mitisha <119412188+mitishaaaa@users.noreply.github.com>
2023-07-31 18:26:54 +0530

    Create ZombieKiller.java

---
+++ arrays/ZombieKiller.java
@@ -0,0 +1,20 @@
+public class ZombieKiller {
+  public static void main(String[] args) {
+    System.out.println("Welcome to our zombie killer program!!!");
+
+  String backpack[] = {"Shotgun", "Rifle", "Sniper"};
+  String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+
+  System.out.println(backpack[1]);
+
+
+
+
+
+
+
+
+
+    
+  }
+}
